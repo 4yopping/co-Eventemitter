7acd0932002e5830f7193eb44e01c26d40c9ef10
cereceres <jesus.edelcereceres@gmail.com>
2016-02-15 20:56:58 -0600

    the once method is added

--- index.js
+++ index.js
@@ -15,9 +15,18 @@ let MyEmitter = function ( ) {
       this.emmitter.removeAllListeners( event )
       this.emmitter.addListener( event, this.events[ event ].eventHandlerFun )
     },
+    this.once = function ( event, eventHandler ) {
+      this.events[ event ] = this.events[ event ] || {}
+      this.events[ event ].eventHandlerGen = this.events[ event ].eventHandlerGen || [ ]
+      this.events[ event ].eventHandlerGen.push( eventHandler )
+      this.events[ event ].eventHandlerFun = funBuilder( this.events[ event ]
+        .eventHandlerGen )
+      this.emmitter.removeAllListeners( event )
+      this.emmitter.once( event, this.events[ event ].eventHandlerFun )
+    },
     this.emit = function ( _event, arg ) {
       arg = arguments.length > 1 ?
-        Array.prototype.slice.call( arguments, 1 ) : [ arg ];
+        Array.prototype.slice.call( arguments, 1 ) : arg;
       return this.emmitter.emit( _event, arg )
     }
   let ctx = this
@@ -42,30 +51,4 @@ let MyEmitter = function ( ) {
 }
 
 let emitter = new MyEmitter( )
-console.log( 'emitter=', emitter.emmitter );
-emitter.on( 'hola', function* ( next, a ) {
-  console.log( 'recibi 1', a );
-  let res = yield Promise.resolve( 'en la primesa 1' )
-  console.log( 'res==', res );
-  yield next
-  console.log( 'regreso promesa 1 ', res );
-  yield Promise.resolve( 4 )
-} )
-emitter.on( 'hola', function* ( next, a ) {
-  console.log( 'recibi 2', a );
-  let res = yield Promise.resolve( 'en la primesa 2' )
-  console.log( 'res==', res );
-  yield next
-  console.log( 'regreso promesa 2 ', res );
-  yield Promise.resolve( 4 )
-} )
-emitter.on( 'hola', function* ( a ) {
-  console.log( 'recibi3', a );
-  let res = yield Promise.resolve( 'en la primesa 3' )
-  console.log( 'res==', res );
-  res = yield Promise.resolve( 4 )
-  console.log( 'regreso promesa 3', res );
-} )
-console.log( 'numero', emitter.emmitter.listenerCount( 'hola' ) );
-emitter.emit( 'hola', 'uno', 'dos' )
 module.exports = MyEmitter
